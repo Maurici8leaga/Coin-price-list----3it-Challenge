0bc298e93c5317967045a57d4dba53ef69b36d98
Mauricio Oleaga <maoa.develoment@gmail.com>
2022-07-04 18:37:55 -0400

    Feat: install react-native-navigation dependencies

---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Intro = () => {
+  return (
+    <View>
+      <Text>Lo que sea que vaya aqui</Text>
+    </View>
+  );
+};
+
+export default Intro;
